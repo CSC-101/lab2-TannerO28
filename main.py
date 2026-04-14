10d30fe96fa676e52d1a07bb43c7e7c281fd9ffd
tannerokeefe_first_commit <twokeefe@calpoly.edu>
2026-04-14 14:53:48 -0700

    Second Commit

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-# Create a welcome message.
-# Input: a name as a string
-# Result: a string
-def welcome_message(name:str) -> str:
-   message = "Hello, " + name + "."
+def smallest(n:float, m:float) -> float:
+    if n < m:
+        return n #Statement is evaluated for calls from first and second
+    else:
+        return m
 
-   return message
+first = smallest(3,2) #first = 2
+second = smallest(2,2) #second = 2
+print(first)
+print(second)
 
+def function2(a: int, b: int, c: int)-> int:
+    if a > b and a > c:
+        return a-b #When a is greater than b and c
+    elif b > c:
+        return b + c   #When a is less than b or c and b is greater than c
+    else:
+        return 2 * c    #When a is less than b or c and b is less than or equal to c
 
-message = welcome_message("twokeefe@calpoly.edu")
-print(message)
+answer1 = function2(3, 2, 1) #answer1 = 1
+answer2 = function2(2, 3, 1)  #answer2 = 4
+answer3 = function2(2, 1, 3)  #answer3 = 6
+print(answer1)
+print(answer2)
+print(answer3)
